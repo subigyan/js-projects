b95b09677393772b1dcc2bf959049045915caca0
Subi <asubigyan@gmail.com>
2021-11-24 10:45:30 +0545

    add main page and add info in readme

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,57 +1,57 @@
 const container = document.getElementById("container");
 const pokeCount = 150;
 const colors = {
-  fire: "#FDDFDF",
-  grass: "#DEFDE0",
-  electric: "#FCF7DE",
-  water: "#DEF3FD",
-  ground: "#f4e7da",
-  rock: "#d5d5d4",
-  fairy: "#fceaff",
-  poison: "#98d7a5",
-  bug: "#f8d5a3",
-  dragon: "#97b3e6",
-  psychic: "#eaeda1",
-  flying: "#F5F5F5",
-  fighting: "#E6E0D4",
-  normal: "#F5F5F5",
+    fire: "#FDDFDF",
+    grass: "#DEFDE0",
+    electric: "#FCF7DE",
+    water: "#DEF3FD",
+    ground: "#f4e7da",
+    rock: "#d5d5d4",
+    fairy: "#fceaff",
+    poison: "#98d7a5",
+    bug: "#f8d5a3",
+    dragon: "#97b3e6",
+    psychic: "#eaeda1",
+    flying: "#F5F5F5",
+    fighting: "#E6E0D4",
+    normal: "#F5F5F5",
 };
 
 const pokeTypes = Object.keys(colors);
 
 const fetchPoke = async () => {
-  for (let i = 1; i < pokeCount; i++) {
-    getPokemonData(i);
-  }
+    for (let i = 1; i < pokeCount; i++) {
+        getPokemonData(i);
+    }
 };
 
 const getPokemonData = async (id) => {
-  pokeApi = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  let res = await fetch(pokeApi);
-  let data = await res.json();
-  createCard(data);
+    pokeApi = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    let res = await fetch(pokeApi);
+    let data = await res.json();
+    createCard(data);
 };
 
 const createCard = (pokemon) => {
-  const pokeElement = document.createElement("div");
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-  const id = pokemon.id.toString().padStart(3, "0");
-  const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
-  const maintype = types[0];
-  pokeElement.style.backgroundColor = colors[maintype];
+    const pokeElement = document.createElement("div");
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const id = pokemon.id.toString().padStart(3, "0");
+    const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
+    const maintype = types[0];
+    pokeElement.style.backgroundColor = colors[maintype];
 
-  let pokeTypesInnerHtml = "";
+    let pokeTypesInnerHtml = "";
 
-  types.forEach((type) => {
-    pokeTypesInnerHtml += `<span">${type} </span>`;
-  });
+    types.forEach((type) => {
+        pokeTypesInnerHtml += `<span">${type} </span>`;
+    });
 
-  console.log(pokeTypesInnerHtml);
+    console.log(pokeTypesInnerHtml);
 
-  const pokeInnerHtml = `
+    const pokeInnerHtml = `
             <div class="img-container">
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/65a610e336f749665c6206f9b733363ca438e54c/sprites/pokemon/other/dream-world/${
-                  pokemon.id
+                    pokemon.id
                 }.svg"
                     alt="" srcset="">
             </div>
@@ -62,10 +62,10 @@ const createCard = (pokemon) => {
                 
             </div>
         `;
-  pokeElement.classList.add("pokemon");
+    pokeElement.classList.add("pokemon");
 
-  pokeElement.innerHTML = pokeInnerHtml;
-  container.appendChild(pokeElement);
+    pokeElement.innerHTML = pokeInnerHtml;
+    container.appendChild(pokeElement);
 };
 
 fetchPoke();
--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Get a random Kanye West Quote
 ## Pokedex
 
 Displays the information of 150 pokemons. Uses async function to display all the information simultaneously.
+
+## Movie DB
+
+Displays movie information. Users can search movies.
+
+## Todo List
+
+Todo list (local storage)
